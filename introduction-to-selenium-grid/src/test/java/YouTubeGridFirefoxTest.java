c5e2d55559fa331c025828b82dee8534da9abeb9
Crypted39 <crypted39@gmail.com>
2022-12-20 20:19:12 +0100

    Adding selenium grid example

---
+++ introduction-to-selenium-grid/src/test/java/YouTubeGridFirefoxTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.testng.Assert.assertTrue;
+
+public class YouTubeGridFirefoxTest {
+
+    @Test
+    public void verifyYouTubeLandingPage() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "firefox");
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://<hub-ip-address-and-port>"), capabilities);
+
+        driver.get("https://youtube.com");
+        assertTrue(driver.findElement(By.id("logo")).isDisplayed());
+        driver.close();
+    }
+}
