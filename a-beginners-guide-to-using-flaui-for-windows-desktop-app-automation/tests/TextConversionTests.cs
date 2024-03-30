18bdfdef66878f6e856b2ffb22c8e279ee332372
Crypted39 <crypted39@gmail.com>
2024-03-30 19:52:34 +0100

    updating the formatting

--- a-beginners-guide-to-using-flaui-for-windows-desktop-app-automation/tests/TextConversionTests.cs
+++ a-beginners-guide-to-using-flaui-for-windows-desktop-app-automation/tests/TextConversionTests.cs
@@ -14,30 +14,23 @@ namespace TextConversionTests {
 
         [TestInitialize]
         public void Setup() {
-            application = Application.Launch(@"C:\Users\IRFAN\Documents\Test Projects\tests\FlaUiTest\resources\textToLowerCase.exe");
+            application = Application.Launch("path-to-your-windows-application.exe");
             mainWindow = application.GetMainWindow(new UIA3Automation());
             cf = new ConditionFactory(new UIA3PropertyLibrary());
         }
 
         [TestMethod]
         public void VerifyApplicationLabel() {
-            Label descriptionLabel = mainWindow.FindFirstDescendant(
-                cf.ByAutomationId("descriptionLabel")
-                ).AsLabel();
+            Label descriptionLabel = mainWindow.FindFirstDescendant(cf.ByAutomationId("descriptionLabel")).AsLabel();
             Assert.AreEqual("Enter some text for the conversion", descriptionLabel.Text);
         }
 
         [TestMethod]
         public void VerifyApplicationConversionFunctionality() {
             string conversionText = "TEst mE out!";
-            mainWindow.FindFirstDescendant(cf.ByAutomationId("inputField"))
-                .AsTextBox()
-                .Enter(conversionText);
-            mainWindow.FindFirstDescendant(cf.ByAutomationId("convertButton"))
-                .AsButton()
-                .Click();
-            Label outputLabel = mainWindow.FindFirstDescendant(cf.ByAutomationId("outputLabel"))
-                .AsLabel();
+            mainWindow.FindFirstDescendant(cf.ByAutomationId("inputField")).AsTextBox().Enter(conversionText);
+            mainWindow.FindFirstDescendant(cf.ByAutomationId("convertButton")).AsButton().Click();
+            Label outputLabel = mainWindow.FindFirstDescendant(cf.ByAutomationId("outputLabel")).AsLabel();
             Assert.AreEqual(conversionText.ToLower(), outputLabel.Text);
         }
 
